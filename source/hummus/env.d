9edb8d42ce6a1802652200857dfb29c5f2149997
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2025-06-29 15:53:56 +0200

    hummus.env
    - Properly implemented

--- source/hummus/env.d
+++ source/hummus/env.d
@@ -6,17 +6,23 @@ import std.process : environment;
 /** 
  * A provider which will look for
  * environment variables based
- *on their name
+ * on a _transformed_ version of
+ * their name. This transformation
+ * replaces all `.` with two underscores -
+ * `__`.
  */
 public class EnvironmentProvider : Provider
 {
     // todo: can '.''s work - I think so?
     protected bool provideImpl(string n, ref string v)
     {
+        import std.string : replace;
+        auto trans_n = replace(n, ".", "__");
+        
         // todo: switch to nothrow version
         try
         {
-            v = environment[n];
+            v = environment[trans_n];
             return true;
         }
         catch(Exception e)
@@ -24,4 +30,28 @@ public class EnvironmentProvider : Provider
             return false;
         }
     }
+}
+
+private version(unittest)
+{
+    import hummus.cfg : fieldsOf;
+}
+
+unittest
+{
+    struct Inner
+    {
+        string z;
+    }
+    
+    struct Cfg
+    {
+        string v;
+        Inner i;
+    }
+    
+    auto cfg = Cfg();
+    
+    // envvars `v` and `i.z` should be present
+    fieldsOf(cfg, new EnvironmentProvider());
 }
