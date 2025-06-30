0fa0ac02891f27022f93e594531da28ed6ced111
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-06-30 13:35:52 +0200

    Update provider.d

--- source/hummus/provider.d
+++ source/hummus/provider.d
@@ -1,7 +1,9 @@
+/**
+ * Definition for the provider interface
+ */
 module hummus.provider;
 
-// todo: make these built in to only non-release builds
-import gogga.mixins;
+import gogga.mixins; // todo: make these built in to only non-release builds
 
 import niknaks.functional : Optional;
 
@@ -14,9 +16,19 @@ import niknaks.functional : Optional;
 public interface Provider
 {
     import std.string : format;
-    import std.stdio : writeln;
 
-    // todo: rename, get
+    /**
+     * The implementation method for a provider.
+     * This must return `true` when an entry by
+     * the name of `n` is found and then set its
+     * associated value via `v_out`. Otherwise,
+     * `false` must be returned.
+     *
+     * Params:
+     *   n = the name being queried
+     *   v_out = the associated value (if found)
+     * Returns: `true` if found, `false` otherwise
+     */
     protected bool provideImpl(string n, ref string v_out);
 
     /**
@@ -34,7 +46,7 @@ public interface Provider
         DEBUG(format("Looking up configuration entry for '%s'...", name));
 
         string _v;
-        if(!provideImpl(name, _v))
+        if (!provideImpl(name, _v))
         {
             ERROR(format("No value mapping for '%s'", name));
             return Optional!(string).empty();
